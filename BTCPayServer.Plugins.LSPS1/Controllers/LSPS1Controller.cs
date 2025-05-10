82d60c0e54188b119156bbe652c7ef6e3af4b0b4
Megalithic.me <info@megalithic.me>
2025-05-10 08:31:10 -0700

    store in route

--- BTCPayServer.Plugins.LSPS1/Controllers/LSPS1Controller.cs
+++ BTCPayServer.Plugins.LSPS1/Controllers/LSPS1Controller.cs
@@ -5,23 +5,36 @@ using BTCPayServer.Plugins.LSPS1.Services;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
-
-
-namespace BTCPayServer.Plugins.LSPS1;
+namespace BTCPayServer.Plugins.LSPS1.Controllers;
 
 [Route("stores/{storeId}/plugins/lsps1")]
-[Authorize(AuthenticationSchemes = AuthenticationSchemes.Cookie,
-          Policy = Policies.CanModifyStoreSettings)]  // <-- ✅ correct constant
+[Authorize(AuthenticationSchemes = AuthenticationSchemes.Cookie)]
 public sealed class LSPS1Controller : Controller
 {
     private readonly LSPS1Service _svc;
     public LSPS1Controller(LSPS1Service svc) => _svc = svc;
 
+    [HttpGet("")]
+    public IActionResult Index(string storeId)
+    {
+        var vm = new PluginPageViewModel { 
+            StoreId = storeId
+        };
+        
+        return View(vm);
+    }
+
     [HttpPost("get-info-connect")]
     [ValidateAntiForgeryToken]
+    [Authorize(Policy = Policies.CanModifyStoreSettings)]
     public async Task<IActionResult> GetInfoAndMaybeConnect(string storeId)
     {
         var (ok, msg) = await _svc.CheckAndConnectAsync(storeId);
         return Json(new { ok, msg });
+    }
+    
+    public class PluginPageViewModel
+    {
+        public string StoreId { get; set; } = string.Empty;
     }
 }
