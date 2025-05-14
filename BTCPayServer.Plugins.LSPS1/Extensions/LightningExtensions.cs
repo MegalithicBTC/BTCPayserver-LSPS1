55b94d6ac05469bcba2112886bcc2ee623b0e94f
perls <info@megalithic.me>
2025-05-14 15:47:56 -0700

    test it now simplified

--- BTCPayServer.Plugins.LSPS1/Extensions/LightningExtensions.cs
+++ BTCPayServer.Plugins.LSPS1/Extensions/LightningExtensions.cs
@@ -1,18 +1,25 @@
 using BTCPayServer.Data;
 using BTCPayServer.Payments;
 using BTCPayServer.Payments.Lightning;
+using BTCPayServer.Services.Invoices;
 using BTCPayServer.Services.Stores;
 using System.Linq;
+using System;
 
 namespace BTCPayServer.Plugins.LSPS1.Services
 {
     public static class LightningExtensions
     {
-        public static string GetLightningUrl(this LightningLikePaymentHandler handler, StoreData store)
+        // Original method signature that the LightningNodeService uses
+        public static string? GetLightningUrl(this LightningLikePaymentHandler handler, StoreData store)
         {
             var paymentMethodId = handler.PaymentMethodId;
-            var config = store.GetPaymentMethodConfig<LightningPaymentMethodConfig>(paymentMethodId);
-            return config?.GetExternalLightningUrl();
+            var config = store.GetPaymentMethodConfig(paymentMethodId);
+            if (config == null)
+                return null;
+            
+            var lightningConfig = handler.ParsePaymentMethodConfig(config) as LightningPaymentMethodConfig;
+            return lightningConfig?.GetExternalLightningUrl();
         }
     }
 }
