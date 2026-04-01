fa0861c5705441cddaf52d2ec384dbbb6210c5f4
Nicolas Dorier <nicolas.dorier@gmail.com>
2026-04-01 21:14:25 +0900

    Bump to .NET10.0

--- BTCPayServer.Plugins.LSPS1/Plugin.cs
+++ BTCPayServer.Plugins.LSPS1/Plugin.cs
@@ -11,8 +11,10 @@ public class Plugin : BaseBTCPayServerPlugin
 {
     public override IBTCPayServerPlugin.PluginDependency[] Dependencies { get; } =
     {
-        new() { Identifier = nameof(BTCPayServer), Condition = ">=1.12.0" }
+        new() { Identifier = nameof(BTCPayServer), Condition = ">=2.3.7" }
     };
+
+    public const string MenuId = "LSP1";
 
     public override void Execute(IServiceCollection services)
     {
